ad928c75dded7e9cc47b165b594c9323962adadf
adamGAOLH <573301642@.qq.com>
2017-12-01 18:13:12 +0800

    header

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,15 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
+    // 箭头函数允许前面不写空格
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi': ['error', 'always'], // 配置有分号
+    'indent': 0, // 忽略编译工具缩略
+    'eol-last': 0, // 不检测文件的末尾是否为空行
+    'space-before-function-paren': 0 // 不需要加空格
   }
 }
--- README.md
+++ README.md
@@ -16,6 +16,17 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+
+添加的package.json
+
+"babel-runtime": "^6.0.0",
+# 是对ES6的语法的转义
+
+"fastclick": "^1.0.6"
+# 是移动端点击300毫秒延迟的问题
+
+"babel-polyfill": "^6.2.0",
+# 补丁的的意思---是对ES6的api转义
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
